a4c3ee5a51b042670a212efcd55c656ac1479036
Mateusz Glock <mateusz.glock@gmail.com>
2024-06-11 00:47:30 +0200

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/MateuszGlock-goit-react-hw-01-components/",
